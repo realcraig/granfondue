637677331b65aadb8628c40b77951b6c6f9d030b
Craig Robinson <realcraig@gmail.com>
2018-12-23 19:31:42 -0800

    fixed date

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,7 +12,7 @@ const Hero = props => {
         <h1>
           <strong>Gran Fondue</strong>
         </h1>
-        <h1>March 2, 2018</h1>
+        <h1>March 2, 2019</h1>
         <button onClick={scrollToContent} aria-label="scroll">
           <FaArrowDown />
         </button>
