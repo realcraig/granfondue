747f045df3f7924daf7782927f8c4fba37d148ab
Craig Robinson <realcraig@gmail.com>
2019-02-09 11:16:19 -0800

    font

--- postcss.config.js
+++ postcss.config.js
@@ -7,7 +7,7 @@ module.exports = ctx => ({
       }
     },
     "postcss-text-remove-gap": {
-      defaultFontFamily: "Open Sans",
+      defaultFontFamily: "Georgia",
       defaultLineHeight: "0"
     },
     "postcss-nested": {},
